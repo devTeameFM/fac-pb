668c8ca15f94bdbeba9386865f7387b4aaa29adc
Matteo Masina <matteo.masina@ad.efmnet.com>
2019-11-05 16:49:31 +0100

    sistemazione modello contratto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fac-pb
+
+TEST 
--- routes/index.js
+++ routes/index.js
@@ -21,11 +21,4 @@ router.get('/surveys', controllers.getAllSurveyDynamics);
 router.get('/surveys/:surveyType', controllers.getAllSurveyByType);
 router.get('/dynamic/:tableName',controllers.getDynamicOptions)
 
-/*
-router.post('/posts', controllers.createPost);
-router.get('/posts', controllers.getAllPosts);
-router.get('/posts/:postId', controllers.getPostById);
-router.put('/posts/:postId', controllers.updatePost);
-router.delete('/posts/:postId', controllers.deletePost);
-*/
 module.exports = router;
